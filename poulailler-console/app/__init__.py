5d87cfc6cfce00349c9e905fdddacc3efc54253a
Laurent REMY <lremy@is4ri.com>
2020-02-04 16:25:36 +0100

    #12 add logs to file

--- poulailler-console/app/__init__.py
+++ poulailler-console/app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='logs/poulailler-console.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 from app import routes

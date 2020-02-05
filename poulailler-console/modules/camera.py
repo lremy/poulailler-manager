c42c10219b81a4b609e037326085d20ecb130471
Laurent REMY <lremy@is4ri.com>
2020-02-05 16:00:23 +0100

    #5 add timestamp to captured photo + keep last 5 only

--- poulailler-console/modules/camera.py
+++ poulailler-console/modules/camera.py
@@ -2,13 +2,14 @@ import picamera
 from datetime import datetime
 from dateutil.tz import *
 from modules.conf import read_conf, write_conf
+from os import listdir, remove
 
 class Camera():
     """classe de pilotage de la caméra du Raspberry Pi"""
     def __init__(self, width, height):
         """initialise la camera"""
         self.camera = picamera.PiCamera()
-        self.capture_path = "static/img/camera.jpg"
+        self.capture_path = "static/img/"
         self.CONF_FILE = "camera"
         self.width = width
         self.height = height
@@ -16,9 +17,16 @@ class Camera():
     def capture(self):
         """capture une image"""
         self.camera.resolution = (self.width, self.height)
-        self.camera.capture("app/" + self.capture_path)
-        self.last_capture = datetime.now(tzlocal()).strftime("%Y-%m-%d %H:%M:%S")
-        return self.capture_path
+        date = datetime.now(tzlocal())
+        self.last_capture = date.strftime("%Y-%m-%d %H:%M:%S")
+        self.last_image = self.capture_path + "camera-{}.jpg".format(date.strftime("%Y%m%d-%H%M%S"))
+        self.camera.capture("app/" + self.last_image)
+        images = list(f for f in listdir("app/" + self.capture_path) if f.startswith("camera-"))
+        images.sort()
+        if len(images) > 5:
+            remove("app/" + self.capture_path + images[0])
+        self.write_config()
+        return self.last_image
 
     def read_config(self):
         """lit le fichier de configuration"""
@@ -29,5 +37,6 @@ class Camera():
         """ecrit la configuration dans le fichier de configuration"""
         cfg = {
             'last_capture':self.last_capture,
+            'last_image':self.last_image,
         }
         write_conf(self.CONF_FILE,cfg)

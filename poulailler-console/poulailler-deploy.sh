d1b67f5220800317cf6c2db82a2fa41252bccbc4
lremy <lremy@is4ri.com>
2020-07-01 20:18:05 +0200

    ajout d'un service pour la gestion de la porte automatique

--- poulailler-console/poulailler-deploy.sh
+++ poulailler-console/poulailler-deploy.sh
@@ -5,8 +5,11 @@ cd /home/pi/poulailler-console
 chmod +x poulailler-service.sh
 
 cp poulailler.service /etc/systemd/system
-
-cp poulailler-console.log-rotate /etc/logrotate.d
-
 systemctl enable poulailler.service
 systemctl start poulailler.service
+
+cp poulailler_porte_auto.service /etc/systemd/system
+systemctl enable poulailler_porte_auto.service
+systemctl start poulailler_porte_auto.service
+
+cp poulailler-console.log-rotate /etc/logrotate.d
